d7c8b1a38ba3272d7271beddedfba13a4b7b209a
bunoviske <brunofcarvalho1996@gmail.com>
2019-09-27 12:09:16 +0200

    created eventListener for posprocessing

--- omniPageServer/documentUnderstanding/posprocess.js
+++ omniPageServer/documentUnderstanding/posprocess.js
@@ -1,3 +1,26 @@
 module.exports = {
     run: run
 }
+
+const bus = require('../eventBus');
+
+function run() {
+    bus.addEventListener("posprocessLuisResponse", findKeyValuePairs)
+}
+
+/********
+@parameters = {
+    bestResults: {
+        label: entity,
+        score: score,
+        type: type,
+        startIndex: startIndex,
+        endIndex: endIndex,
+        luisSentenceMapObject: luisSentenceMapObject
+    }
+}
+********/
+function findKeyValuePairs(parameters) {
+    console.log(parameters.bestResults);
+}
+
