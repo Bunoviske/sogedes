3bcf9381d4b2ef31899f20a5405b6fd78ec26064
bunoviske <brunofcarvalho1996@gmail.com>
2019-10-25 12:11:30 +0200

    new url from luis app

--- omniPageServer/documentUnderstanding/luisApi/httpRequest.js
+++ omniPageServer/documentUnderstanding/luisApi/httpRequest.js
@@ -10,8 +10,15 @@ const { _, performance } = require('perf_hooks');
 const debug = require('./debugWithoutApi');
 const sysHandler = require('../../fileSystemHandler/fileSystemHandler')
 
-const genericSentenceLuisUrl = 'https://westeurope.api.cognitive.microsoft.com/luis/v2.0/apps/1e35ceda-dfc9-4a94-8535-85a788a63a64?verbose=true&timezoneOffset=0&subscription-key=21ddb7c530d841c59feb5f7d8b8d28e1&q=';
-const continuousTextLuisUrl = 'https://westeurope.api.cognitive.microsoft.com/luis/v2.0/apps/7a89d70d-fb31-48b0-b6e0-0a330757dd10?verbose=true&timezoneOffset=0&subscription-key=21ddb7c530d841c59feb5f7d8b8d28e1&q=';
+// Free Tier
+// const genericSentenceLuisUrl = 'https://westeurope.api.cognitive.microsoft.com/luis/v2.0/apps/1e35ceda-dfc9-4a94-8535-85a788a63a64?verbose=true&timezoneOffset=0&subscription-key=21ddb7c530d841c59feb5f7d8b8d28e1&q=';
+// const continuousTextLuisUrl = 'https://westeurope.api.cognitive.microsoft.com/luis/v2.0/apps/7a89d70d-fb31-48b0-b6e0-0a330757dd10?verbose=true&timezoneOffset=0&subscription-key=21ddb7c530d841c59feb5f7d8b8d28e1&q=';
+
+// Standard Tier
+const genericSentenceLuisUrl = 'https://westeurope.api.cognitive.microsoft.com/luis/v2.0/apps/1e35ceda-dfc9-4a94-8535-85a788a63a64?verbose=true&timezoneOffset=0&subscription-key=1fe11ea8af6f4792a87a32a93b8f198d&q=';
+const continuousTextLuisUrl = 'https://westeurope.api.cognitive.microsoft.com/luis/v2.0/apps/7a89d70d-fb31-48b0-b6e0-0a330757dd10?verbose=true&timezoneOffset=0&subscription-key=1fe11ea8af6f4792a87a32a93b8f198d&q=';
+
+
 
 let responseCounter = 0; 
 
@@ -87,7 +94,7 @@ function httpRequest(urlLuis, query, listenerFunction) {
         resp.on('end', () => {
             // console.log(data);
             sysHandler.getFileSystemHandler("logHandler").appendLogFile("Http response from LUIS: \r\n" + data + "\r\n\r\n");
-            // console.log(`statusCode: ${resp.statusCode}`)
+            if (parseInt(resp.statusCode) == 429 ) console.log(`Http Error: ${resp.statusCode}`);
             listenerFunction({ data: data }); //call response listener callback
         });
 
